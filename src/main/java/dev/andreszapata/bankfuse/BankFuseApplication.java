69189f92733edf40638c74b415f5d150c85a26be
Andres Zapata <andres.zapata@oljsolutions.com>
2024-04-11 17:40:11 -0500

    init proyect

---
+++ src/main/java/dev/andreszapata/bankfuse/BankFuseApplication.java
@@ -0,0 +1,13 @@
+package dev.andreszapata.bankfuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankFuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankFuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/andreszapata/bankfuse/BankFuseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andreszapata.bankfuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankFuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
